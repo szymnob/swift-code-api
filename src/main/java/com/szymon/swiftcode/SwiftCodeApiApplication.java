53595a424f547a19d8c2e1590cf9cc7a4b99ffc1
szymnob <szymon6457@gmail.com>
2025-04-10 13:48:11 +0200

    initial commit

---
+++ src/main/java/com/szymon/swiftcode/SwiftCodeApiApplication.java
@@ -0,0 +1,13 @@
+package com.szymon.swiftcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwiftCodeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwiftCodeApiApplication.class, args);
+	}
+
+}
